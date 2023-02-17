380534731e3b3cee265cab8791f4e1b772315639
Abraham Ayamigah <abkc070@gmail.com>
2023-02-17 06:18:46 -0800

    Some bugs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: 0 Success
+ * Return: 1 Success
  */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (0);
+return (1);
 }
