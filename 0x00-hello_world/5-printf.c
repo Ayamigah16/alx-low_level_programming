4032d2b6f9e5fcc45424c3c65660b4dda748a8ed
Abraham Ayamigah <abkc070@gmail.com>
2023-02-16 17:45:18 -0800

    Some bugs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - A program that prints a line using the printf function
  * Return: (0) 
- */
+*/
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
