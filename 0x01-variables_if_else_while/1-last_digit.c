53c94d92688eaabc88c065af4bb6e18f7ad6d451
Abraham Ayamigah <abkc070@gmail.com>
2023-02-18 07:26:01 -0800

    Some bugs

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 else
 {
-printf("Last  digit of %d is %d and is 0\n", n, m);
+printf("Last digit of %d is %d and is 0\n", n, m);
 }
 return (0);
 }
