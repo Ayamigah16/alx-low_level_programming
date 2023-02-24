ea300351b0dbe9c89e947320a18b3e99f604d3a6
Abraham Ayamigah <abkc070@gmail.com>
2023-02-24 15:45:07 -0800

    Some bugs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,7 @@
  * prints_times_table - prints the n times table,  starting with 0
  * @n: number of the times table
  */
-void print_time_table(int n)
+void print_times_table(int n)
 {
 int i, j, k;
 
