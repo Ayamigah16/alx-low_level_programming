65ebf35b730f47bac841cb3f296d2004063fb24b
Abraham Ayamigah <abkc070@gmail.com>
2023-03-07 14:28:59 -0800

    C pointers, arrays and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 int index;
 if (*needle == 0)
 {
-return (hasystack);
+return (haystack);
 }
 
 while (*haystack)
@@ -26,7 +26,7 @@ if (haystack[index] == needle[index])
 do {
 if (needle[index + 1] == '\0')
 {
-return (hasystack);
+return (haystack);
 }
 index++;
 } while (haystack[index] == needle[index]);
