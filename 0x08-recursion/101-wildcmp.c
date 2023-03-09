4fc039f6b5218cb0fd37fcc7fd2aa61ee136a416
Abraham Ayamigah <abkc070@gmail.com>
2023-03-08 17:40:20 -0800

    C recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,39 +1,42 @@
 #include "main.h"
+
 /**
- * wildcmp - compares two strings and returns 1 if they can be considered
- *           identical, otherwise returns 0. The `s2` parameter can contain
- *           the special character `*`, which can replace any string
- * @s1: the first string to compare
- * @s2: the second string to compare, which can contain `*`
+ * str_checker - check if two strings are identical.
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
+ * Return: 1 if s is palindrome, 0 otherwise.
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+if (s1[i] == '\0' && s2[j] == '\0')
+{
+return (1);
+}
+if (s1[i] == s2[j])
+{
+return (str_checker(s1, s2, i + 1, j + 1));
+}
+if (s1[i] == '\0' && s2[j] == '*')
+{
+return (str_checker(s1, s2, i, j + 1));
+}
+if (s2[j] == '*')
+{
+return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+}
+return (0);
+}
+
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string.
+ * @s2: base address for string.
  *
- * Return: 1 if the strings can be considered identical, otherwise 0
+ * Return: 1 if are considered identical.
  */
 int wildcmp(char *s1, char *s2)
 {
-/* If both strings are empty, they are identical */
-if (*s1 == '\0' && *s2 == '\0')
-{
-return (1);
-}
-/* If the current character of s2 is *, try all possible matches with s1 */
-if (*s2 == '*')
-{
-/* Skip the * in s2 */
-s2++;
-/* If s2 is now empty, it matches any string */
-if (*s2 == '\0')
-{
-return (1);
-}
-/* Try all possible matches of s1 with the rest of s2 */
-return ((*s1 == '\0' && *s2 != '\0') ? 0 :
-(wildcmp(s1 + ((*s1 == *s2 || *s2 == '?') ? 1 : 0), s2) ||
-wildcmp(s1, s2 + 1)));
-}
-/* If the current character of s2 is not *, compare it with s1 */
-if (*s1 == '\0' || (*s1 != *s2 && *s2 != '?'))
-{
-return (0);
-}
-return (wildcmp(s1 + 1, s2 + 1));
+return (str_checker(s1, s2, 0, 0));
 }
