b0f00681934d230c2414e0c3f1295621f2c8ce57
Abraham Ayamigah <abkc070@gmail.com>
2023-03-08 16:09:37 -0800

    C recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,6 +15,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int wildcap(char *s1, char *s2);
+int wildcmp(char *s1, char *s2);
 
 #endif
