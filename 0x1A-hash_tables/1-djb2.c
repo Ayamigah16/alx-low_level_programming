06b50dac56ef9711c021950c2a1aa1f7d6efaa89
Abraham Ayamigah <abkc070@gmail.com>
2023-09-09 20:11:28 -0700

    task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,7 +3,7 @@
 /**
  * hash_djb2 - hash function implementing the dijb2 algorithm
  * @str: string used to generate the hash value
- * Rerurn: hash value
+ * Return: hash value
 */
 unsigned long int hash_djb2(const unsigned char *str)
 {
