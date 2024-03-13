4dd6b0a2c90b5ec5a9011342280f0e9013f60023
Abraham Ayamigah <abkc070@gmail.com>
2024-03-13 16:26:54 -0700

    11. Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,45 +1,64 @@
-#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * advanced_binary - Searches for a value in a
- * sorted array using Advanced Binary search
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * @value: Value to search for
+ * rec_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * Return: Index where value is located, or -1 if
- * not present or array is NULL
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int rec_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
+}
+
+/**
+ * advanced_binary - calls to rec_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-size_t low = 0, high = size - 1;
-int index = -1;
-size_t i;
-size_t mid;
+	int index;
 
-if (array == NULL)
-{return -1; }
+	index = rec_search(array, size, value);
 
-while (low <= high)
-{
-mid = (low + high) / 2;
+	if (index >= 0 && array[index] != value)
+		return (-1);
 
-printf("Searching in array: ");
-for (i = low; i < high; i++)
-{printf("%d, ", array[i]); }
-printf("%d\n", array[high]);
-
-if (array[mid] == value)
-{
-index = mid;
-high = mid - 1;
-}
-else if (array[mid] < value)
-low = mid + 1;
-else
-high = mid - 1;
-}
-
-{return index; }
+	return (index);
 }
